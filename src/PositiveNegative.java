79f749870318e73b3d689b85fcd98101da3dd26e
ramya276 <32501427+ramya276@users.noreply.github.com>
2017-10-27 13:36:43 -0700

    Positive Negative Solution

--- src/PositiveNegative.java
+++ src/PositiveNegative.java
@@ -1,11 +1,27 @@
-public class PositiveNegative {
+import java.util.Scanner;
 
-	/*
-	 * define a program that finds if a number is positive or negative.
-	 * 
-	 * The program should print positive if the number is positive, negative if it is negative
-	 */
-	public static void main(String args[]){
+/*
+		 * define a program that finds if a number is positive or negative.
+		 * 
+		 * The program should print positive if the number is positive, negative if it is negative
+		 */
+	
+public class PositiveNegative {
+		int num=console.nextInt();
+		if(num<0)
+		public static void main(String args[]){
+	
+		Scanner console=new Scanner(System.in);
+		System.out.println("Enter Number:");
+		{
+			System.out.println("Negative");
+		}
+		else
+			System.out.println("Positive");
+
+		console.close();
 		
+		}
 	}
-}
+
+
