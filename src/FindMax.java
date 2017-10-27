41d72e61007dbe4e51f9dd3c6a6a65d583ed74a4
ramya276 <32501427+ramya276@users.noreply.github.com>
2017-10-27 13:23:23 -0700

    FindMax Solution

--- src/FindMax.java
+++ src/FindMax.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
 
-public class FindMax {
 
 	/*
 	 * Define a program that finds the maximum number when 2 numbers are given.
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class FindMax {
+	
 
+	public static void main(String[] args) {
+	
+		Scanner console=new Scanner(System.in);
+		System.out.println("Enter Number 1: ");
+		int num1=console.nextInt();
+		System.out.println("Enter Number 2: ");
+		int num2=console.nextInt();
+		if (num1>num2)
+		{
+			System.out.println(num1 + " is the Max");
+		}
+		else 
+			System.out.println(num2+ " is the max");
+		}
 	}
 
-}
